98f3524e59505f9372160920ea3aac31e98f2177
Sunilkr1 <Ssunilkr87800@gmail.com>
2026-05-16 15:52:44 +0530

    Fix PORT

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(errorHandler);
 
 // Server
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT) || 8080
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🤖 NexaBot Backend running on port ${PORT}`);
 });
